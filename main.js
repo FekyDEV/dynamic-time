41d6508ff26ecfa242b590ffd2ff388d1d0ed84c
FekyDEV <71899549+FekyDEV@users.noreply.github.com>
2022-01-29 21:44:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+/**
+* @name Dynamic Time
+* @description Convert classic time to dynamic time for Discord.. (JavaScript)
+* @author FekyDEV
+* @authorId 603505971507101698
+* @version 1.0.0
+* @invite sKKEyUn
+* @source https://github.com/FekyDEV/multi-language/new/main
+* @license MIT
+*/
+
 // Converting Date to Number for Discord dynamic time !
 d = new Date();
-let datum = d.getTime()
-const first_change = datum.toString()
-const second_change = first_change.slice(0,10)
-const date_to_num = parseInt(second_change)
-
+  let date = d.getTime()
+    const first = date.toString()
+    const second = first.slice(0,10)
+    const date_to_num = parseInt(second)
+    
 console.log("New time:", date_to_num)
 
 // In discord: `<t:${date_to_num}>`
