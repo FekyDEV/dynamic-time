c8df961dac81ada8c5dbba07033fa91335fd0f32
FekyDEV <71899549+FekyDEV@users.noreply.github.com>
2022-02-13 12:25:08 +0100

    Update, now it looks better :)

--- main.js
+++ main.js
@@ -9,11 +9,18 @@
 * @license MIT
 */
 
-// Converting Date to Number for Discord dynamic time !
-d = new Date();
-  let date = d.getTime()
-    const first = date.toString()
-    const second = first.slice(0,10)
-    const date_to_num = parseInt(second)
-    
-console.log("New time: %s \n For Discord: <t:%s>", date_to_num, date_to_num)
+// Converting Date to Number for Discord dynamic time.
+const date = new Date();
+const new_date = parseInt(date.getTime().toString().slice(0,10))
+
+console.log("New time: %s \nFor Discord: <t:%s>", new_date, new_date)
+
+/*
+ TYPES:
+ - <t:123:R> => R: Relative, says "two weeks ago", or "in 5 years"
+ - <t:123:D> => Date, says "July 4, 2021"
+ - <t:123:T> => Time, "11:28:27 AM"
+ - <t:123:t> => Short Time, "11:28 AM"
+ - <t:123:F> => Full, "Monday, July 4, 2021 11:28:27 AM"
+ */
+
